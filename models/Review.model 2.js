026198256e8b773887d58035e30b9aea4a97601f
David Hatch <hatchdavidv@gmail.com>
2023-01-19 16:45:00 -0600

    Se modificaron archivos de modelos

---
+++ models/Review.model 2.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const reviewSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    coment: {
+      type: String,
+      required: true,
+
+    },
+    
+
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Review = model("review", reviewSchema);
+
+module.exports = Review;
