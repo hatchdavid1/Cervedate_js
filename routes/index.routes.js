89ba7b7385eec4134d08823e210277d0f9742f2a
KRcamara <113925794+KRcamara@users.noreply.github.com>
2023-01-21 08:52:53 -0600

    se le dio formato a la pagina y se conectaron los botones de inicio

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+
+const isLoggedOut = require("../middleware/isLoggedOut");
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/login", isLoggedOut, (req, res) => {
+  res.render("auth/login");
+});
+
+router.get("/signup", isLoggedOut, (req, res) => {
+  res.render("auth/signup");
+});
 module.exports = router;
